fbc7b92e91f6c79bc2e72f821bf60fdec36a228c
Ade Adeola <adedotster@gmail.com>
2025-04-18 00:00:45 +0100

    feat: add Support component with contact email

---
+++ app/(main)/support.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Support() {
+  return (
+    <View>
+      <Text>You can reach me through email at adedotster@gmail.com</Text>
+    </View>
+  );
+}
